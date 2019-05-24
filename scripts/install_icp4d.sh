5863f233355017f09c2ecf3b130d13a8c4637014
Simon Shi <simon.shi@ibm.com>
2019-05-24 03:01:10 -0400

    #6227 fix, icp4d install check on server retry case

--- scripts/install_icp4d.sh
+++ scripts/install_icp4d.sh
@@ -2,12 +2,30 @@
 image_icp4d_uri=$1
 image_location_icp4d=$2
 
+function check_install() {
+    local pidFile=/var/run/icp4d.pid
+
+    while [[ -f $pidFile ]];do
+        if which docker > /dev/null 2>&1 && docker ps | grep dp-installer > /dev/null 2>&1;then
+            docker_logs=$(docker logs --tail 10 dp-installer | grep -Ei 'fail' | grep -Ei 'retry' | grep -Ei 'skip')
+            if [[ -n "$docker_logs" ]];then
+                echo "ICP4D Installer experiencing issue and waiting for user input, You may review the installer log under /ibm/InstallPackage/tmp"
+                kill -9 $(cat $pidFile)
+                rm -rf $pidFile
+                break
+            fi
+        fi
+        sleep 10
+    done
+}
+
 awscli=$(which aws > /dev/null 2>&1)
 if [[ $? -ne 0 ]];then
     if [[ -e "/usr/local/bin/aws" ]];then
         awscli="/usr/local/bin/aws"
     fi
 fi
+
 if [[ "$awscli" != "" ]] && [[ "${image_location_icp4d}" != "" ]];then
     echo "downloading icp4d installer"
     installer_name=$(basename $image_location_icp4d)
@@ -23,9 +41,17 @@ if [[ "$awscli" != "" ]] && [[ "${image_location_icp4d}" != "" ]];then
     fi
     cd /ibm
     chmod a+x $installer_name
+
+    pidFile=/var/run/icp4d.pid
+    rm -rf $pidFile
+    echo $$ > $pidFile
+    check_install &
+
     ./$installer_name --load-balancer --accept-license
     if [[ $? -ne 0 ]];then
         echo "error installing icp4d,please check log under /ibm/InstallPacakge/tmp for details"
         exit 1
     fi
+
+    rm -rf $pidFile
 fi
